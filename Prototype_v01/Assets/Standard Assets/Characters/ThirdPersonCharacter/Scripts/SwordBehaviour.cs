0236faf2000fa85a8011ccf920b93c0b8d4c8d42
SrUrn <ianeito95@gmail.com>
2016-01-17 19:56:29 +0100

    Prototype_test

---
+++ Prototype_v01/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/SwordBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordBehaviour : MonoBehaviour {
+
+	public Transform trans;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("q")) {
+			trans.localScale = new Vector3 (0.1f, 0.1f, 2);
+			//trans.position = new Vector3 (0.287f, 0.127f, -0.96f);
+
+		}
+	
+	}
+}
