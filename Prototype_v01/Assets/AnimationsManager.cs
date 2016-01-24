01ffbd0e8b6b4782a34a9977f24474ad67cd4685
SrUrn <ianeito95@gmail.com>
2016-01-24 17:34:30 +0100

    Animation_Attack_1
    sustitucion de la escena anterior, por una nueva con otro personaje y
    una animacion de ataque

---
+++ Prototype_v01/Assets/AnimationsManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationsManager : MonoBehaviour {
+
+    public AnimationClip attack;
+    public Animation myAnim;
+
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+
+        myTransform = this.transform;
+
+        myAnim = myTransform.GetComponent<Animation>();
+
+        myAnim.Stop();
+
+        myAnim.clip = attack;
+
+        myAnim.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
