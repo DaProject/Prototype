7b574b860fd729fdddc8b4c25713f006b1d84aae
Puxi22 <alexpuighibetdesigns@gmail.com>
2016-01-24 20:12:53 +0100

    sdfghj

--- Prototype_v01/Assets/Resources/Scripts/Character/PrototypePlayer/AnimationsManager.cs
+++ Prototype_v01/Assets/Resources/Scripts/Character/PrototypePlayer/AnimationsManager.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class AnimationsManager : MonoBehaviour {
 
     public AnimationClip attack;
+	public AnimationClip idle;
+	public AnimationClip run;
+	public AnimationClip death;
+
     public Animation myAnim;
 
     Transform myTransform;
@@ -15,16 +19,51 @@ public class AnimationsManager : MonoBehaviour {
 
         myAnim = myTransform.GetComponent<Animation>();
 
-        myAnim.Stop();
+		//myAnim.Stop();
 
-        myAnim.clip = attack;
+		//myAnim.clip = idle;
 
-        myAnim.Play();
+        //myAnim.Play();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (Input.GetKeyUp (KeyCode.Keypad1)) {
+
+			myAnim.Stop();
+
+			myAnim.clip = idle;
+
+			myAnim.Play();
+		}
+
+		if (Input.GetKeyUp (KeyCode.Keypad2)) {
+
+			myAnim.Stop();
+
+			myAnim.clip = run;
+
+			myAnim.Play();
+		}
+
+		if (Input.GetKeyUp (KeyCode.Keypad3)) {
+
+			myAnim.Stop();
+
+			myAnim.clip = attack;
+
+			myAnim.Play();
+		}
+
+		if (Input.GetKeyUp (KeyCode.Keypad4)) {
+
+			myAnim.Stop();
+
+			myAnim.clip = death;
+
+			myAnim.Play();
+		}
 	}
 }
