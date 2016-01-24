3b183c5fa6c4559605f3fbaaff6de69cb040b83b
Puxi22 <alexpuighibetdesigns@gmail.com>
2016-01-24 23:58:12 +0100

    Adding Scripts and Animation Controller
    Scripts Added: - PlayerHealth
    - EnemyAttack

---
+++ Prototype_v01/Assets/Resources/Scripts/Character/PlayerHealth.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public float flashSpeed = 5f;
+	public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+	Animator anim;
+	PlayerMovement playerMovement;
+
+	bool damaged;
+	bool isDead;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent <Animator> ();
+		playerMovement = GetComponent <PlayerMovement> ();
+		currentHealth = startingHealth;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (damaged) {
+			damageImage.color = flashColour;
+		} 
+		else 
+		{
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+	}
+
+	public void TakeDamage (int amount)
+	{
+		damaged = true;
+
+		currentHealth -= amount;
+
+		healthSlider.value = currentHealth;
+
+		if (currentHealth <= 0 && !isDead) 
+		{
+			Death ();
+		}
+			
+	}
+
+	void Death ()
+	{
+		isDead = true;
+
+		anim.SetTrigger ("Death");
+
+		playerMovement.enabled = false;
+
+	}
+}
---
+++ Prototype_v01/Assets/Resources/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+
+	public float timeBetweenAttacks = 0.5f;
+	public int attackDamage = 10;
+
+
+	Animator anim;
+	GameObject player;
+	PlayerHealth playerHealth;
+
+	bool playerInRange;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		anim = GetComponent <Animator> ();
+
+
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == player) 
+		{
+			playerInRange = true;
+		}
+	
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject == player) 
+		{
+			playerInRange = false;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		timer += Time.deltaTime;
+
+		if (timer >= timeBetweenAttacks && playerInRange)
+		{
+			Attack ();
+		}
+		/*
+		if (playerHealth.currentHealth <= 0) 
+		{
+			anim.SetTrigger ("PlayerDeath");
+		}
+		*/
+	}
+		
+	void Attack ()
+	{
+		timer = 0f;
+
+		if (playerHealth.currentHealth > 0) 
+		{
+			playerHealth.TakeDamage (attackDamage);
+		}
+	}
+}
