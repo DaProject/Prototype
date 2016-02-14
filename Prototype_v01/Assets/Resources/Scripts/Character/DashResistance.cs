8c12dc14a2d8ba18171cbb2a8849506091febb19
Puxi22 <alexpuighibetdesigns@gmail.com>
2016-02-14 02:11:06 +0100

    Added sounds and DashResistance bar

---
+++ Prototype_v01/Assets/Resources/Scripts/Character/DashResistance.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DashResistance : MonoBehaviour {
+
+	public int maxDashResistance;
+	public int currentDashResistance;
+	public int resistancePerDash;
+	public Slider DashResistanceSlider;
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentDashResistance = maxDashResistance;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (DashResistanceSlider.value < 100) 
+		{
+			DashResistanceSlider.value ++;
+		}
+
+		if (DashResistanceSlider.value >= resistancePerDash) 
+		{
+			if (Input.GetKeyDown (KeyCode.LeftShift)) {
+				DashResistanceSlider.value -= resistancePerDash;
+			}
+		}
+	}
+}
--- Prototype_v01/Assets/Resources/Scripts/Character/PlayerManager.cs
+++ Prototype_v01/Assets/Resources/Scripts/Character/PlayerManager.cs
@@ -39,10 +39,17 @@ public class PlayerManager : MonoBehaviour
     // Dash
     [Header("Dash")]
     public float speedDash;                         // The speed wich the player makes a dash.
+	public int maxDashResistance;
+	public int currentDashResistance;
+	public int resistancePerDash;
+	public Slider DashResistanceSlider;
 
     //Sounds
     [Header("Sounds")]
     public AudioClip hurtClip;
+	public AudioClip lowHpClip;
+	public AudioClip deathClip;
+	public AudioClip swordSwipeClip;
     AudioSource playerAudio;
 
 	// UI Player
@@ -107,6 +114,13 @@ public class PlayerManager : MonoBehaviour
 				VictoryBehaviour();
 				break;
 		}
+
+		if (DashResistanceSlider.value < 100) DashResistanceSlider.value ++; 
+		if (currentHealth <= 10) 
+		{
+			playerAudio.clip = lowHpClip;
+			playerAudio.Play();  
+		}
 	}
 
 	// Behaviours
@@ -128,7 +142,7 @@ public class PlayerManager : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.Alpha1) && SlashActive) setSlash();
 
-        if (Input.GetKeyDown(KeyCode.LeftShift)) setDash();     // Calls the setDash function if left shift key is pressed.
+		if ((Input.GetKeyDown(KeyCode.LeftShift)) && (DashResistanceSlider.value >= resistancePerDash)) setDash();     // Calls the setDash function if left shift key is pressed.
     }
 
     private void Attack10Behaviour()
@@ -188,6 +202,8 @@ public class PlayerManager : MonoBehaviour
     { 
         currentHealth = maxHealth;                      // Sets the player health to the value of maxHealth that you indicated.
 
+		currentDashResistance = maxDashResistance;
+
 		SlashActive = false;
 
         playerAudio = GetComponent<AudioSource>();      // Gets the component AudioSource from the player.
@@ -220,6 +236,9 @@ public class PlayerManager : MonoBehaviour
 
         anim.SetTrigger("Attack10");                    // Plays the attack10 animation.
 
+		playerAudio.clip = swordSwipeClip;
+		playerAudio.Play();  
+
         state = PlayerStates.ATTACK_10;                 // Goes to the attack10 state.
     }
 
@@ -249,6 +268,8 @@ public class PlayerManager : MonoBehaviour
     {
         Debug.Log("Dash");
 
+		DashResistanceSlider.value -= resistancePerDash;
+
         anim.SetTrigger("IsDashing");                           // Plays the dash animation.
 
         rigidBody.AddForce(transform.forward * speedDash);      // Adds a force to the rigidbody of the player, in order of doing a dash.
@@ -265,6 +286,7 @@ public class PlayerManager : MonoBehaviour
 
 		currentHealth -= damage;                                // Applies the damage recieved.
 
+		playerAudio.clip = hurtClip;
         playerAudio.Play();                                     // Plays the hurt sound when the player gets hit.
 
         healthSlider.value = currentHealth;                     // Sets the value of the slider from the currentHealth of the player.
@@ -279,7 +301,7 @@ public class PlayerManager : MonoBehaviour
 
         anim.SetTrigger("Die");                                 // Plays the die animation.
 
-        playerAudio.clip = hurtClip;                            // Plays the hurt sound when you get hit.
+        playerAudio.clip = deathClip;                            // Plays the hurt sound when the player dies.
         playerAudio.Play();
 
 		state = PlayerStates.DEAD;                              // Calls the DEAD state.
